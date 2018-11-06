e0e391252d059a7460a6101086d6489bf70f5d79
SabikAbtahee <bsse0829@iit.du.ac.bd>
2018-11-06 22:55:32 +0600

    Virtual environment

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sabik\google~1\study\iit5th~1\spl2ra~1\project2\byte-p~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
